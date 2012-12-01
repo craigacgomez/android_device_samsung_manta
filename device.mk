77a59c214d953a7db3ad3b3c801037e4b4f965d1
Craig Gomez <craigacgomez@gmail.com>
2012-11-30 22:01:58 -0800

    Some overlay make & overlay updates. New overclocked kernel

--- device.mk
+++ device.mk
@@ -210,7 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.secure=0 \
     ro.debuggable=1 \
     persist.sys.usb.config=mtp \
     persist.service.adb.enable=1
